2a68ccd69686a2133cb86b6549b36e101aeac974
Anthony Romrell <anthonyromrell@mac.com>
2020-02-05 11:17:12 -0700

    Updated Character Controllers

--- Project1/Assets/Scripts/CharacterMover.cs
+++ Project1/Assets/Scripts/CharacterMover.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMover : MonoBehaviour
 {
-
     public CharacterController controller;
-    private Vector3 positionDirection;
-    public float speed = 5f;
-    public float gravity = -3f;
-    public float jumpForce = 10f;
+    public Vector3 positionDirection;
+    public float speed = 10f;
+    public float gravity = 3f;
+    public float jumpForce = 30f;
     
     void Start()
     {
@@ -17,11 +16,13 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        
+        if (Input.GetButtonDown("Jump"))
         {
             positionDirection.y = jumpForce;
         }
-        positionDirection.y += gravity;
+        
+        positionDirection.y -= gravity;
         controller.Move(positionDirection*Time.deltaTime);
     }
 }
