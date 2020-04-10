aa91d1830b1086d571a00b38a7fca56c255ed798
Anthony Romrell <anthonyromrell@mac.com>
2020-04-09 18:02:56 -0600

    Health and Damage.

---
+++ Project1/Assets/Scripts/HealthAndDamage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthAndDamage : MonoBehaviour
+{
+    public int damagePower = 10;
+    public int health = 100;
+    public Text healthText;
+    public float attackTime = 1f;
+    private WaitForSeconds waitObj;
+    private bool canAttack;
+
+    private void Start()
+    {
+        healthText.text = health.ToString();
+        waitObj = new WaitForSeconds(attackTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        var otherHealthAndDamage = GetComponent<HealthAndDamage>();
+        
+        canAttack = true;
+        while (canAttack)
+        {
+            otherHealthAndDamage.health -= damagePower;
+            healthText.text = health.ToString();
+            yield return waitObj;
+            if (health <= 0)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canAttack = false;
+    }
+}
