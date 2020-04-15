25e9d967d31461c112fbf276f187bf50a37c4d0f
Anthony Romrell <anthonyromrell@mac.com>
2020-04-15 11:19:26 -0600

    Platform

---
+++ Project1/Assets/Scripts/OddEven.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class OddEven : MonoBehaviour
+{
+    void Start()
+    {
+        var counter = 10;
+        while (counter > 0)
+        {
+            if(counter % 2 == 0)
+            {
+                print(counter + " is even");
+            }
+            else
+            {
+                print(counter + " is odd");
+            }
+            counter--;
+        }
+    }
+}
