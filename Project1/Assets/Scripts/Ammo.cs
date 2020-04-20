e3fcf2feef03c04ce87b6dda54831d75b87f92e6
Anthony Romrell <anthonyromrell@mac.com>
2020-04-20 14:10:36 -0600

    Instancing, coroutines.

--- Project1/Assets/Scripts/Ammo.cs
+++ Project1/Assets/Scripts/Ammo.cs
@@ -3,19 +3,18 @@
 [RequireComponent(typeof(Rigidbody))]
 public class Ammo : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 10f;
     private Rigidbody rigidBod;
 
     void Start()
     {
         rigidBod = GetComponent<Rigidbody>();
-        rigidBod.velocity = transform.right * speed;
+        rigidBod.velocity = transform.forward * speed;
         Destroy(gameObject, 2f);
     }
 
     private void OnTriggerEnter(Collider hitInfo)
     {
-        Debug.Log(hitInfo.name);
         Destroy(gameObject);
     }
 }
